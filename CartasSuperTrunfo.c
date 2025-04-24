f2eeabe796e185bc95417ab1fcfbc63d12c21bd2
joao ph <llipe.9175@gmail.com>
2025-04-24 15:38:12 +0000

    quase lá, eu acho!!!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,13 +5,13 @@ int main(){
     printf("Desafio super trunfo iniciante\n");
 
     printf("Carta 1: %d\n",1);
-    printf("Estado: %c\n", A);
-    printf("Código: ")
-    printf("Nome da Cidade: \n")
-    printf("População: \n")
-    printf("Área: \n")
-    printf("PIB: \n")
-    printf("Número de Pontos Turísticos: \n")
+    printf("Estado: %c\n",a);
+    printf("Código: ");
+    printf("Nome da Cidade: \n");
+    printf("População: \n");
+    printf("Área: \n");
+    printf("PIB: \n");
+    printf("Número de Pontos Turísticos: \n");
 
 
 
---
+++ tentativa 2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Desafio super trunfo iniciante \n");
+    char estado[15],codigo[5],cidade[10];
+    int populaçao,turisticos;
+    float area,PIB;
+
+    printf("carta 1\n");
+
+    printf("Digite o nome estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo: \n");
+    scanf("%s", &codigo);
+
+    printf("Digite o nome cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite o numero da populaçao: \n");
+    scanf("%d", &populaçao);
+
+    printf("Digite numeros de pontos turisticos: \n");
+    scanf("%d", &turisticos);
+
+    printf("Digite a area: \n");
+    scanf("%d", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%d", &PIB);
+
+    printf("carta 2\n");
+
+    printf("Digite o nome estado: \n");
+    scanf("%s", &estado);
+
+    printf("Digite o codigo: \n");
+    scanf("%s", &codigo);
+
+    printf("Digite o nome cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digite o numero da populaçao: \n");
+    scanf("%d", &populaçao);
+
+    printf("Digite numeros de pontos turisticos: \n");
+    scanf("%d", &turisticos);
+
+    printf("Digite a area: \n");
+    scanf("%d", &area);
+
+    printf("Digite o PIB: \n");
+    scanf("%d", &PIB);
+
+    
+
+
+
+    
+}
